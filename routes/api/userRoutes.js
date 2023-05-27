d44ad3eab3236edaf3b78f25621e03d1004cfff3
Milywayz <jtjenks@gmail.com>
2023-05-26 20:25:18 -0500

    added userRoutes and finished

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const {
+    getUsers,
+    getOneUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+  } = require('../../controllers/userController');
+  
+  // /api/users
+  router.route('/').get(getUsers).post(createUser);
+  
+  // /api/users/:userId
+  router.route('/:userId')
+  .get(getOneUser)
+  .put(updateUser)
+  .delete(deleteUser)
+
+  router.route('/:userId/friends/:friendId')
+  .post(addFriend)
+  .delete(removeFriend)
+  
+  module.exports = router;
