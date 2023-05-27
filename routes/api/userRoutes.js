73124a0bb11f357d95f8b91b752a96d663767c45
Milywayz <jtjenks@gmail.com>
2023-05-26 20:34:39 -0500

    added thought route

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -18,8 +18,10 @@ const {
   .put(updateUser)
   .delete(deleteUser)
 
-  router.route('/:userId/friends/:friendId')
+  router.route('/:userId/friends')
   .post(addFriend)
+
+  router.route('/:userId/friends/:friendId')
   .delete(removeFriend)
   
   module.exports = router;
